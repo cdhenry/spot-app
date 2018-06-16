5aa6bcfee268a490d7e48fc594116382fc45e9af
cdhenry <cdhenry@gmail.com>
2018-06-16 11:30:32 -0400

    added recursive solution in comments

--- question3.js
+++ question3.js
@@ -11,3 +11,19 @@ function changePossibilities(amount, denominations) {
   })
   return combinations[amount]
 }
+
+
+// Recursively
+// function changePossibilities(amount, denominations, current = 0) {
+//   if(amount === 0){
+//     return 1
+//   } else if (amount < 0){
+//     return 0
+//   } else {
+//     let possibilities = 0
+//     for(let i = current; i < denominations.length; i++){
+//       possibilities += changePossibilities(amount - denominations[i], denominations, i)
+//     }
+//     return possibilities
+//   }
+// }
